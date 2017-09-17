43b127a14ae432142f5907e3672cd12d60f68d05
Fintan Halpenny <rossmanf@tcd.ie>
2017-09-17 17:24:25 +0100

    Added section and coiter

--- src/Cofree.hs
+++ src/Cofree.hs
@@ -4,6 +4,24 @@ module Cofree where
 
 import           Control.Comonad (Comonad (..))
 
+
+-- | extend :: (f a -> b) -> f a -> f b
+-- | substitute section in the function
+-- | extend :: (f a -> Cofree f a) -> f a -> f (Cofree f a)
+section :: Comonad f => f a -> Cofree f a
+section f = extract f :< extend section f
+
+-- | a :< is the obvious first move
+-- | Then we need to use `k` somehow, so we do `k a`
+-- | That gives us an `f a`
+-- | We need a `Cofree f a`, so how can we get there? Recursion of course!
+-- | So we have `a` in the `f a` and if we do `coiter k` we have something
+-- | that goes from `a` to `Cofree f a`
+-- | We also know `f` is a Functor so we can do:
+-- | fmap (coiter k) $ k a
+coiter :: Functor f => (a -> f a) -> a -> Cofree f a
+coiter k a = a :< fmap (coiter k) (k a)
+
 data Cofree f a =
   a :< (f (Cofree f a))
 
